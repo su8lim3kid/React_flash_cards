40e3aadb58edfbc6363d83b065bf51326c189ca6
Angel Alderete <aldereteangel21@gmail.com>
2019-12-05 16:38:00 -0700

    edit works

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,24 +1,50 @@
 import React from "react"
 import Post from "./Post"
 import {Header,} from "semantic-ui-react"
+import PostForm from "./PostForm"
 
 class Deck extends React.Component{
     state={
         posts:[
-            {id:1 , title:"question", body:"answer"},
-            {id:2 , title:"test", body:"answertest"}
+            {id:1 , title:"Question", body:"Answer"},
+            {id:2 , title:"Question 2", body:"Answer 2"}
 
         ]
     }
+    getId = () => {
+        return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1)
+    }
+
+    addPost =(postData) =>{
+        const {posts,} = this.state
+        const post = {id: this.getId(), ...postData}
+        this.setState({posts: [post, ...posts], }) 
+    }
 
 renderPosts = () => {
-    return this.state.posts.map( post => <Post key={post.id} {...post} />)
+    return this.state.posts.map( post => <Post key={post.id} edit={this.editPost} {...post} />)
 }
+
+editPost = (postData) => {
+    const posts = this.state.posts.map( post => {
+      if (post.id === postData.id)
+        return postData;
+      return post
+    });
+    this.setState({ posts, });
+  }
+
+  renderPosts = () => {
+    return this.state.posts.map( post => <Post key={post.id} {...post} edit={this.editPost} />)
+  };
 
 render() { 
     return (
         <div>
             <Header as="h1">Deck</Header>
+            <PostForm add={this.addPost}toggleEdit={this.toggleEdit} />
             {this.renderPosts()}
         </div>
 
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,21 +1,44 @@
-import React from "react"
-import {Card, Button,} from "semantic-ui-react"
+import React, {Fragment} from "react"
+import {Card, Button, } from "semantic-ui-react"
+import PostForm from './PostForm'
 
 class Post extends React.Component{
+    state = {
+        editing: false,
+    }
+
+    toggleEdit = () => this.setState({ editing: !this.state.editing, });
+
     render(){
         return(
+
+            
+           
             <Card>
+                { this.state.editing ?
+                <Fragment>
+                    <PostForm title={this.props.title} body={this.props.body} id={this.props.id}
+                    edit={this.props.edit} toggleEdit={this.toggleEdit} />
+            </Fragment>
+            :
+            <>
             <Card.Content header={ this.props.title } />
             <Card.Content description={this.props.body } />
+            </>
+                }
+            
+           
+            
             <Card.Content extra>
             <Button color='teal'>Show </Button>
-            <Button color='purple'>Edit </Button>
+            <Button color='purple' onClick={this.toggleEdit}>Edit </Button>
             <Button color='red'>Delete </Button>
               
             </Card.Content>
           </Card>
+        
         )
-    }
+            }
 }
 
 
